b2e32d9d1ea4b1d69a2f463efe61fc74af90a908
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-21 20:44:59 -0300

    fix: Componente HelloWorld e importando Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <nav>Pruebaaaa</nav>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Soy goooodines"/>
+    
   </div>
 </template>
 
 <script>
 
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
