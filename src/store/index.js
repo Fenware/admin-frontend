b1f51194c6ffe3da62d4c65300170d0462744dfe
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-22 19:19:59 -0300

    feat: Añadiendo la url de api al state

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,11 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    API_URL: process.env.VUE_APP_ROOT_API
   },
+  /* getters:{
+    getApiUrl: state
+  }, */
   mutations: {
   },
   actions: {
