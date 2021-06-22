c0c5af9b54c4534e31f2290426a1c302dbedc7a6
MateConAzucar <fds.workmail@gmail.com>
2021-06-21 23:33:58 -0300

    feat: added background and grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
-<template>
-  <div id="nav">
+<template> 
+  <div class="page">
     <Navbar/>
+    <div></div>
+    <div></div>
   </div>
 
   <router-view/>
@@ -17,5 +19,35 @@ export default {
 
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+:root{
+  font-size: 62.5%;
+  font-family: 'Open Sans', sans-serif;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  min-height: 100vh;
+  background: linear-gradient(to right top ,#175AD4,#65DF9D);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.page{
+  width: 90vw;
+  height: 90vh;
+  display: grid;
+  grid-template-columns: 6% 69% 20%;
+  gap : 1%;
+  & > *{
+    background: green;
+    border-radius: 1.5rem;
+  }
+}
+
 
 </style>
