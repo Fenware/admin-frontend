ad1a8e8e93804164f64d2b1e8ba5a37143d7e9b8
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-22 15:31:36 -0300

    fix: Error al importar componente Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import Home from '../views/Login.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
   { path: '/', redirect: '/home' },
