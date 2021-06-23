5cadf5d99293ae87eceac3fa1ce05f6e44956e3a
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-23 18:00:20 -0300

    feat: Metodo login, setToken, syncToken y state de token

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,43 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from 'axios';
 
 export default createStore({
   state: {
-    API_URL: process.env.VUE_APP_ROOT_API
+    API_URL: process.env.VUE_APP_ROOT_API,
+    token: null,
   },
-  /* getters:{
-    getApiUrl: state
-  }, */
   mutations: {
+    setToken(state, payload) {
+      state.token = payload;
+    },
   },
   actions: {
+    async login({ commit }, payload) {
+      console.log(payload);
+      try {
+        await axios
+          .post(this.state.API_URL + "/auth", payload, {headers: {"Content-Type": "application/json"} })
+          .then((res) => {
+            var token = res.data.result.token;
+            commit("setToken", token);
+            localStorage.setItem('token', token);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+          window.location.href = "http://localhost:8081/home";
+
+      }catch (error) {
+        console.log("error: ", error);
+      }
+    },
+    syncToken({ commit }) {
+      if (localStorage.getItem("token")) {
+        commit("setToken", localStorage.getItem("token"));
+      } else {
+        commit("setToken", null);
+      }
+    },
   },
-  modules: {
-  }
-})
+});
