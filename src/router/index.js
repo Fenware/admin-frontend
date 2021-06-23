855640430a4a938e2437f441ddbc5023c1636720
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-23 17:59:12 -0300

    feat: Protegiendo ruta home

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Login from '../views/Login.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import store from '../store';
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
   { path: '/', redirect: '/home' },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: "/home",
+    name: "Home",
+    component: Home,
+    meta: { requireAuth: true },
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const rutaProtegida = to.matched.some((record) => record.meta.requireAuth);
+  
+  //console.log(rutaProtegida);
+
+  if (rutaProtegida && store.state.token === null) {
+    // ruta protegida es true
+    // token es nulo true, por ende redirigimos al inicio
+    next({ name: "Login" });
+  } else {
+    // En caso contrario sigue...
+    next();
+  }
 });
 
 export default router;
