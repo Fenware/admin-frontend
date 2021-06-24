8ce6667e2f3a77f7a43d1546a448228bd5b2d1b9
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-24 00:30:14 -0300

    feat: Nuevos posibles fondos

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,12 @@ export default {
 
 body {
   /* background: linear-gradient(to right top ,#175AD4,#65DF9D); */
-  background-image: url(https://4kwallpapers.com/images/wallpapers/macos-big-sur-apple-layers-fluidic-colorful-dark-wwdc-2020-5120x2880-1432.jpg);
+
+  // Posibles fondos
+
+  background-image: url(https://4kwallpapers.com/images/wallpapers/macos-big-sur-apple-layers-fluidic-colorful-dark-wwdc-2020-5120x2880-1432.jpg);  
+  /* background-image: url(https://4kwallpapers.com/images/wallpapers/macos-monterey-wwdc-21-stock-5k-6016x6016-5584.jpg); */
+  /* background: url(https://dl.dropbox.com/s/mpmu0gjtxv2x3fs/Webp.net-resizeimage%20%281%29.jpg?raw=1) no-repeat center center fixed;*/
   background-size: cover;
   background-position: center;
   min-height: 100vh;
