4fa8a29e1b1fdc4d4114d32e1fdef3261dcc373d
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-24 00:14:47 -0300

    feat: Nuevo background, clase bg-glass mejorada

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,48 @@
-<template> 
+<template>
   <div class="">
-
-   <router-view/>
-
+    <router-view />
   </div>
-
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import { mapActions } from "vuex";
 export default {
   methods: {
-    ...mapActions(['syncToken'])
+    ...mapActions(["syncToken"]),
   },
-  created(){
-    this.syncToken()
-  }
-}
+  created() {
+    this.syncToken();
+  },
+};
 </script>
 
-
 <style lang="scss">
-
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
-:root{
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");
+:root {
   font-size: 62.5%;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 
-*{
+* {
   margin: 0;
   padding: 0;
 }
 
-
-body{
-  background: linear-gradient(to right top ,#175AD4,#65DF9D);
+body {
+  /* background: linear-gradient(to right top ,#175AD4,#65DF9D); */
+  background-image: url(https://4kwallpapers.com/images/wallpapers/macos-big-sur-apple-layers-fluidic-colorful-dark-wwdc-2020-5120x2880-1432.jpg);
+  background-size: cover;
+  background-position: center;
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.bg-glass{
-  background: rgba( 255, 255, 255, 0.10 );
-  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-  backdrop-filter: blur( 4.5px );
-  -webkit-backdrop-filter: blur( 4.5px );
-  border-radius: 10px;
-  border: 1px solid rgba( 255, 255, 255, 0.18 );
+.bg-glass {
+  background-color: rgba(255, 255, 255, 0.06);
+  -webkit-backdrop-filter: blur(2.5rem);
+  backdrop-filter: blur(2.5rem);
+  border-radius: 1.2rem;
 }
 </style>
