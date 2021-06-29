c34de681b43266e29c7a508f0c37e2376084461e
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-29 18:48:00 -0300

    feat: Le agregue un titulo al sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-glass">
-
+    <h1>Conectados</h1>
   </div>
 </template>
 
@@ -10,6 +10,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+h1 {
+  color: #fff;
+  font-size: 2.2rem;
+  text-align: center;
+  margin-top: 0.8rem;
+  /*  margin-bottom: 2rem;
+ */
+}
 </style>
