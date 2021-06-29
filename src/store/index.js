653f5b19f93f91437039d546244becc8e08d26e3
lucaspintos909 <lucaspintos909@gmail.com>
2021-06-29 18:49:26 -0300

    feat: Materias y su mutacion para editar el nombre

--- src/store/index.js
+++ src/store/index.js
@@ -6,19 +6,34 @@ export default createStore({
   state: {
     API_URL: process.env.VUE_APP_ROOT_API,
     token: null,
+    subjects: [
+      { id: 1, name: "Matematica", state: 1 },
+      { id: 2, name: "Programacion", state: 1 },
+      { id: 3, name: "Base de datos", state: 1 },
+      { id: 4, name: "Diseño web", state: 1 },
+      { id: 5, name: "Analisis y Diseño de aplicaciones", state: 1 },
+      { id: 6, name: "Gestion de Proyecto", state: 1 },
+    ],
   },
   mutations: {
     setToken(state, payload) {
       state.token = payload;
     },
+    changeSubjectName(state, subject){
+      console.log(subject.name);
+      state.subjects.forEach((item, index) => {
+        if(item.id == subject.id){
+          item.name = subject.name;
+        }
+      });
+    }
   },
   actions: {
     async login({ commit }, payload) {
+      console.log(payload);
       try {
         await axios
-          .post(this.state.API_URL + "/auth", payload, {
-            headers: { "Content-Type": "application/json" },
-          })
+          .post(this.state.API_URL + "/auth", payload)
           .then((res) => {
             var token = res.data.result.token;
             commit("setToken", token);
@@ -45,6 +60,6 @@ export default createStore({
       commit("setToken", null);
       localStorage.removeItem("token");
       router.push('login');
-    },
+    }
   },
 });
