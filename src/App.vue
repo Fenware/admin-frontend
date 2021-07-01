2df96f78231f0dbc607ae794b3d3b3d6a5153d39
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-01 14:04:49 -0300

    feat: Nuevo background svg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
-  
-  <router-view/>
+  <div class="" v-if="currentRouteName() == 'Login'">
+    <router-view />
+  </div>
+  <div class="page" v-else>
+    <!-- <Navbar />
+    <Main />
+    <Sidebar /> -->
+  </div>
 </template>
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  components: {},
+  methods: {
+    ...mapActions(["syncToken"]),
+    currentRouteName() {
+      return this.$route.name;
+    },
+  },
+  created() {
+    this.syncToken();
+  },
+};
+</script>
 
 <style>
+body {
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap');
+  font-family: 'Nunito', sans-serif;
+  /* background: linear-gradient(to right top ,#175AD4,#65DF9D); */
+  background-image: url("./assets/background.svg");
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+}
 </style>
