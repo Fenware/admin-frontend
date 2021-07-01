dbce64d2ec4e2f6eda77c1fc783999197670e1b7
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-01 14:05:22 -0300

    feat: Ruta Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import store from '../store';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
+  { path: "/", redirect: "/home" },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+    path: "/inicio",
+    name: "Home",
+    component: Home,
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/ingresar",
+    name: "Login",
+    component: Login,
+    meta: { requireAuth: false },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
   const rutaProtegida = to.matched.some((record) => record.meta.requireAuth);
-  
+
   //console.log(rutaProtegida);
 
   if (rutaProtegida && store.state.token === null) {
