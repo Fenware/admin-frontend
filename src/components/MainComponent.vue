97ff772c67a882a249e37f1d9c11199757c42538
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-02 14:51:11 -0300

    feat: MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="">
+    <div class="" v-if="isLoginView()">
+      <router-view />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+  methods: {
+    isLoginView() {
+      return this.$route.name != "Login";
+    },
+  },
+};
+</script>
+
+<style></style>
