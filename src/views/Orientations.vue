50bce3dbd6308aed916402bb048ddafddd016d33
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-02 23:29:56 -0300

    feat: Titulo y sincronizando orientaciones

--- src/views/Orientations.vue
+++ src/views/Orientations.vue
@@ -1,10 +1,20 @@
 <template>
-  <div></div>
+  <div class="text-white w-full h-full">
+    <h2 class="text-center text-3xl mt-1">Orientaciones</h2>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "Orientetions",
+  methods:{
+    ...mapActions(['checkSession', 'syncOrientations'])
+  },
+  created(){
+    this.checkSession();
+    this.syncOrientations();
+  }
 };
 </script>
 
