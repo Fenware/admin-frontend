326131d44e9ba9bbe85be46afb1cc174a8759f1f
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-07 19:52:50 -0300

    refactor: Centrando contenedor de materias

--- src/components/SubjectsContainer.vue
+++ src/components/SubjectsContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex overflow-auto flex-wrap md:max-w-2xl lg:max-w-3xl mt-20 bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-xl"
+    class="flex overflow-auto flex-wrap mx-auto md:max-w-2xl lg:max-w-3xl mt-20 bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-xl"
   >
     <div v-for="subject in subjectsFiltered" :key="subject.id">
       <div
