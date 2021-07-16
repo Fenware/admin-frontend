1b0fc6aac070196f0baebad54cea18f1f8dee50f
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-16 00:32:20 -0300

    refactor: Cambiando id del for

--- src/components/SubjectsContainer.vue
+++ src/components/SubjectsContainer.vue
@@ -2,7 +2,7 @@
   <div
     class="flex overflow-auto flex-wrap mx-auto md:max-w-2xl lg:max-w-3xl mt-20 bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-xl"
   >
-    <div v-for="subject in subjectsFiltered" :key="subject.id">
+    <div v-for="(subject, index) in subjectsFiltered" :key="index + subject.id">
       <div
         :id="subject.id + 'no_edit_mode'"
         class=" block m-3 px-3 py-1 bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl rounded-full"
