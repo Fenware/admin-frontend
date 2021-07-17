8d59b338273f42d7b0442baa379b3fb21d2c100f
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-17 16:52:48 -0300

    refactor: Quitando codigo innecesario

--- src/components/OrientationsContainer.vue
+++ src/components/OrientationsContainer.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="  m-3 py-3 w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-2xl"
+    class=" max-h-28 m-3 py-3 w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-2xl"
     v-for="(orientation, index) in orientations"
     :key="orientation.id + index"
   >
-    <div class="">
+    <div>
       <OrientationCard :orientation="orientation" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState } from "vuex";
 import OrientationCard from "@/components/OrientationCard";
 
 export default {
@@ -22,14 +22,10 @@ export default {
   data: function() {
     return {
       edit_mode: false,
-      orientation_data: { name: "", year: null, subjects: [] },
     };
   },
   computed: {
-    ...mapState(["API_URL", "headers", "user", "subjects", "orientations"]),
-  },
-  methods: {
-    ...mapActions(["createOrientation"]),
+    ...mapState(["API_URL", "headers", "orientations"]),
   },
 };
 </script>
