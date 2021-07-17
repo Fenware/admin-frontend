959b834a872ba802e14361955fd7cba0ed9b081b
lucaspintos909 <lucaspintos909@gmail.com>
2021-07-17 16:52:16 -0300

    refactor: Estilo nuevo para la card, quitando codigo innecesario

--- src/components/OrientationCard.vue
+++ src/components/OrientationCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="">
-    <div class="flex px-2 justify-between">
+    <div class="flex px-2 justify-between ">
       <span
-        class="py-1 px-2 w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-lg"
+        class="py-1 px-2 w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-lg "
         >{{ orientation.name }}</span
       >
       <span
@@ -10,27 +10,37 @@
         >Año: {{ orientation.year }}</span
       >
     </div>
-    <div class="mt-2 ml-2 flex justify-between">
-      <div div class="flex">
+    <div class="mt-3 ml-2 flex justify-between items-center">
+      <div div class="flex overflow-x-auto max-w-lg ">
         <div
-          class="flex items-center"
+          class="flex-none"
           v-for="(subject, index) in orientationSubjctsFiltered"
           :key="subject.id_subject + index"
         >
           <p
-            class="px-2 mx-1 bg-white bg-opacity-20 backdrop-filter backdrop-blur-xl shadow-2xl rounded-full"
-            v-if="parseInt(subject.id_orientation) == parseInt(orientation.id)"
+            class="px-2 mx-1 bg-white bg-opacity-20 backdrop-filter backdrop-blur-xl shadow-2xl rounded-full select-none"
+            v-if="
+              parseInt(subject.id_orientation) == parseInt(orientation.id)
+            "
           >
             {{ subject.name }}
           </p>
         </div>
-      </div> 
-      <button
-        @click="modifyOrientation()"
-        class="py-1 px-5 mr-2 | transition-colors duration-200 bg-blue-700  bg-opacity-50 hover:bg-opacity-70 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-full"
-      >
-        Editar
-      </button>
+      </div>
+      <div class="flex ml-1">
+        <button
+          @click="modifyOrientation()"
+          class="py-1 px-5 mr-2 | transition-colors duration-200 bg-blue-700  bg-opacity-50 hover:bg-opacity-70 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-full"
+        >
+          Editar
+        </button>
+        <button
+          @click="deleteOrientation(parseInt(orientation.id))"
+          class="py-1 px-5 mr-2 | transition-colors duration-200 bg-red-700  bg-opacity-50 hover:bg-opacity-70 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-full"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -43,35 +53,22 @@ export default {
   props: {
     orientation: Object,
   },
-  /* data: function() {
-    return { orientation_subjects: this.orientation_subjects };
-  }, */
+  data: function() {
+    return {
+      count : 0
+    };
+  },
   computed: {
     ...mapState(["API_URL", "headers"]),
     ...mapGetters(["orientationSubjctsFiltered"]),
   },
   methods: {
     ...mapMutations(["toogleModifyOrientationMode", "setOrientation"]),
-    ...mapActions(["getOrientationSubjects"]),
-
-    generateSubjectsArray(orientation_subjects_data) {
-      orientation_subjects_data.forEach((orientation_subject) => {
-        this.subjects.forEach((subject) => {
-          if (orientation_subject.id_subject == parseInt(subject.id)) {
-            this.orientation_subjects.push(subject);
-          }
-        });
-      });
-    },
+    ...mapActions(["getOrientationSubjects",'deleteOrientation']),
     modifyOrientation() {
       this.toogleModifyOrientationMode();
       this.setOrientation(this.orientation);
     },
-  },
-  created() {
-    /* this.orientation_subjects = this.getOrientationSubjects(parseInt(this.orientation.id)); */
-    /* this.getOrientationSubjects(parseInt(this.orientation.id)); */
-    /* this.syncOrientationSubjects(); */
   },
 };
 </script>
