057eaeb83a920316806f72f78baa39b0debe4bdb
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-01 19:41:18 -0300

    refactor: Mejorando el enrutamiento

--- src/router/index.js
+++ src/router/index.js
@@ -7,60 +7,76 @@ import Orientations from "../views/Orientations.vue";
 import Groups from "../views/Groups.vue";
 
 const routes = [
-    { path: "/", redirect: "/inicio" },
-    {
-        path: "/inicio",
-        name: "Home",
-        component: Home,
-        meta: { requireAuth: true },
+  {
+    path: "/",
+    redirect: "/inicio",
+  },
+  {
+    path: "/inicio",
+    name: "Home",
+    component: Home,
+    meta: {
+      requireAuth: true,
     },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
-        meta: { requireAuth: false },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {
+      requireAuth: false,
     },
-    {
-        path: "/materias",
-        name: "Subjects",
-        component: Subjects,
-        meta: { requireAuth: true },
+  },
+  {
+    path: "/materias",
+    name: "Subjects",
+    component: Subjects,
+    meta: {
+      requireAuth: true,
     },
-    {
-        path: "/orientaciones",
-        name: "Orientations",
-        component: Orientations,
-        meta: { requireAuth: true },
+  },
+  {
+    path: "/orientaciones",
+    name: "Orientations",
+    component: Orientations,
+    meta: {
+      requireAuth: true,
     },
-    {
-        path: "/grupos",
-        name: "Groups",
-        component: Groups,
-        meta: { requireAuth: true },
+  },
+  {
+    path: "/grupos",
+    name: "Groups",
+    component: Groups,
+    meta: {
+      requireAuth: true,
     },
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
-    const routeProtected = to.matched.some((record) => record.meta.requireAuth);
-    let redirectedFrom = to.redirectedFrom;
-    /* console.log(redirectedFrom);
-    console.log(to); */
-    if (routeProtected) {
-        // ruta protegida es true
-        // Verificando la session cada vez que se entra a una ruta protegida
-        store.dispatch('checkSession');
-        next();
-    } else if (to.fullPath === "/login" && store.state.token !== null) {
-        // En caso contrario sigue...
-        next({ name: redirectedFrom != undefined ? redirectedFrom : "Home" });
-    } else {
-        next();
+  const routeProtected = to.matched.some((record) => record.meta.requireAuth);
+  /* let redirectedFrom = to.redirectedFrom; */
+  /* console.log(redirectedFrom);
+  console.log(to); */
+  // Verificando la session en cada ruta
+  store.dispatch('syncToken');
+  if (routeProtected) {
+    store.dispatch("checkSession");
+    if (store.state.token !== null) {
+      next();
     }
+  } else if (to.fullPath == "/login") {
+    if (store.state.token !== null) {
+      next({ name: "Home" });
+    } else {
+      next();
+    }
+  }
 });
 
 export default router;
