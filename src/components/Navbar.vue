82d3ec82fa7659f1c77f4cdb783835fe4934b6cd
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-10 23:48:04 -0300

    refactor: Cambiando color de fondo al nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="w-1/6 md:w-16 lg:w-16 h-full rounded-3xl | bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl"
+    class="w-1/6 md:w-16 lg:w-16 h-full rounded-2xl | bg-gray-700 border-r-2 border-t-2 border-b-2 border-gray-700  bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl"
   >
     <ul class="pt-4 h-full text-center flex flex-col justify-between">
       <div>
@@ -23,6 +23,11 @@
           <router-link :to="{ name: 'Groups' }" class="cursor-pointer">
             <i :class="'fa-users ' + icon_class"></i>
           </router-link>
+        </li>
+        <li class="cursor-pointer">
+          <router-link :to="{ name: 'User' }" class="cursor-pointer">
+            <i :class="'fa-user ' + icon_class"></i>
+            </router-link>
         </li>
       </div>
       <div>
