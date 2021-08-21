91b3eb3ff114ed4630437567b05a81e781791e68
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-21 20:31:00 -0300

    refactor: Limpiando el store de funciones para grupos

--- src/store/index.js
+++ src/store/index.js
@@ -10,9 +10,6 @@ export default createStore({
       Authorization: "",
       "Content-Type": "application/json",
     },
-    text_filter: "",
-    create_group_mode: false,
-    modify_group_mode: false,
   },
   mutations: {
     setToken(state, payload) {
@@ -20,49 +17,7 @@ export default createStore({
     },
     setHeaderToken(state, payload) {
       state.headers.Authorization = payload;
-    },
-    setText(state, payload) {
-      state.text_filter = payload;
-    },
-    toogleCreateGroupMode(state) {
-      state.create_group_mode = !state.create_group_mode;
-    },
-    toogleModifyGroupMode(state) {
-      state.modify_group_mode = !state.modify_group_mode;
-    },
-    setGroups(state, groups) {
-      state.groups = groups;
-    },
-    setGroup(state, group) {
-      state.group = group;
-    },
-    addGroup(state, group) {
-      state.groups.push(group);
-    },
-    editGroup(state, modified_group) {
-      /* state.groups.find(
-        (group) => parseInt(group.id) == parseInt(modified_group.id)
-      ).name = modified_group.name; */
-      state.groups.forEach((group) => {
-        if (group.id == modified_group.id) {
-          group.name = modified_group.name;
-          group.id_orientation = modified_group.orientacion;
-
-          let orientation_data = state.orientations.find(
-            (orientation) =>
-              parseInt(orientation.id) == parseInt(modified_group.orientacion)
-          );
-          group.orientation_name = orientation_data.name;
-        }
-      });
-    },
-    removeGroup(state, group_data) {
-      state.groups.forEach((group, index) => {
-        if (group.id == group_data.id) {
-          state.groups.splice(index, 1);
-        }
-      });
-    },
+    }
   },
   actions: {
     searcher({ commit }, payload) {
