33622716dbcdd1cc330c612b8ebc9ce36fd5afaf
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-21 13:38:12 -0300

    feat: Modificar grupo, limpiando logs, quitando comentarios

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -4,74 +4,57 @@
       Grupos
     </h2>
 
-    <ListGroup v-show="mode == 'list'" :groups="groups" @changeMode="changeMode" @deleteGroup="deleteGroup"/>
-    <CreateGroup v-if="mode == 'create'" :orientations="orientations" @changeMode="changeMode" @addGroup="addGroup"/>
-
-
-    <!-- <div class="flex justify-between mt-10">
-      <input
-        type="text"
-        placeholder="Buscar grupo"
-        v-model="orientation_filter"
-        class="w-96 mx-2 py-2 px-2 | bg-white transition duration-300 focus:bg-opacity-20 hover:bg-opacity-20 bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl | rounded-2xl  outline-none placeholder-white"
-      />
-      <button
-        v-show="!create_group_mode && !modify_group_mode"
-        @click="toogleCreateGroupMode()"
-        class="ml-4 pr-2 | bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl transition duration-300 focus:bg-opacity-20 hover:bg-opacity-20 shadow-2xl | rounded-2xl"
-      >
-        <i
-          class="fas fa-plus text-white text-md py-3 px-3 | filter drop-shadow-xl transition-transform duration-300 transform hover:scale-110"
-        ></i>
-        Agregar grupo
-      </button>
-    </div>
-
-    <CreateGroupContainer class="mt-2" v-if="create_group_mode" :orientations="orientations"/>
-    <ModifyGroupContainer v-if="modify_group_mode" />
-    <div
-      class="flex overflow-auto h-2/3 mx-auto p-1 flex-wrap md:max-w-2xl lg:max-w-3xl mt-5 bg-white bg-opacity-10  shadow-2xl | rounded-lg"
-    >
-      <GroupsContainer />
-    </div> -->
+    <ListGroup
+      v-show="mode == 'list'"
+      :groups="groups"
+      @changeMode="changeMode"
+      @deleteGroup="deleteGroup"
+    />
+    <CreateGroup
+      v-if="mode == 'create'"
+      :orientations="orientations"
+      @changeMode="changeMode"
+      @addGroup="addGroup"
+    />
+    <EditGroup
+      v-if="mode == 'edit'"
+      :orientations="orientations"
+      :group="group"
+      @changeMode="changeMode"
+      @modifyGroup="modifyGroup"
+    />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import { mapState, mapMutations } from "vuex";
-/* import GroupsContainer from "@/components/GroupsContainer";
-import CreateGroupContainer from "@/components/CreateGroupContainer";
-import ModifyGroupContainer from "@/components/ModifyGroupContainer"; */
 import ListGroup from "@/components/groups/ListGroup";
 import CreateGroup from "@/components/groups/CreateGroup";
-/* import EditGroup from "@/components/groups/EditGroup"; */
+import EditGroup from "@/components/groups/EditGroup";
 
 export default {
-  name: "Orientations",
+  name: "Groups",
   data: function() {
     return {
+      orientations: [],
       groups_filter: "",
       groups: [],
-      mode: "list"
+      group: {},
+      mode: "list",
     };
   },
   components: {
     ListGroup,
     CreateGroup,
-    /* EditGroup */
-/*     GroupsContainer,
-    CreateGroupContainer,
-    ModifyGroupContainer, */
+    EditGroup,
   },
   created() {
     this.getGroups();
+    this.getOrientations();
   },
   computed: {
-    ...mapState([
-      "API_URL",
-      "headers"
-    ]),
+    ...mapState(["API_URL", "headers"]),
   },
   methods: {
     ...mapMutations([
@@ -79,11 +62,26 @@ export default {
       "toogleCreateGroupMode",
       "setGroups",
     ]),
-    changeMode(mode, orientation) {
+    changeMode(mode, group) {
       this.mode = mode;
-      if(orientation){
-        this.orientation = orientation;
+      if (group) {
+        this.group = group;
       }
+    },
+    async getOrientations() {
+      await axios({
+        method: "get",
+        url: this.API_URL + "/orientacion",
+        headers: this.headers,
+      })
+        .then((res) => {
+          if (Array.isArray(res.data)) {
+            this.orientations = res.data;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     async getGroups() {
       await axios({
@@ -93,9 +91,11 @@ export default {
       })
         .then((res) => {
           if (Array.isArray(res.data)) {
-            console.log(res.data);
-            res.data.forEach(group => {
-              group.full_name = (group.year == "1" || group.year == "3" ? group.year + "ero" : group.year + "do") + ` ${group.name}`;
+            res.data.forEach((group) => {
+              group.full_name =
+                (group.year == "1" || group.year == "3"
+                  ? group.year + "ero"
+                  : group.year + "do") + ` ${group.name}`;
               this.groups.push(group);
             });
           } else {
@@ -106,8 +106,20 @@ export default {
           console.log(error);
         });
     },
-    addGroup(group){
+    addGroup(group) {
       this.groups.push(group);
+    },
+    modifyGroup(modified_group) {
+      this.groups.forEach((group) => {
+        if (group.id == modified_group.id) {
+          group.id = modified_group.id;
+          group.name = modified_group.name;
+          group.year = modified_group.orientation.year;
+          group.full_name = modified_group.full_name;
+          group.id_orientation = modified_group.orientation.id;
+          group.orientation_name = modified_group.orientation.name;
+        }
+      });
     },
     async deleteGroup(id, group_name, group_year) {
       var data = {
@@ -124,7 +136,7 @@ export default {
           if (res.data == 1) {
             // Elimino el objeto del array
             this.groups.forEach((group, index) => {
-              if(parseInt(group.id) == parseInt(id)){
+              if (parseInt(group.id) == parseInt(id)) {
                 this.groups.splice(index, 1);
               }
             });
@@ -134,7 +146,7 @@ export default {
               icon: "info",
               title: `El grupo ${group_year}${group_name} fue eliminado correctamente!`,
             });
-          }else{
+          } else {
             this.$swal({
               icon: "error",
               title: `El grupo ${group_year}${group_name} no pudo ser eliminado, actualice la página e intente nuevamente`,
@@ -146,7 +158,6 @@ export default {
         });
     },
   },
-
 };
 </script>
 
