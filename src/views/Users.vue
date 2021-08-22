87d290a4ec25460a8e1e056ee924a8ab346632d8
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-22 00:22:19 -0300

    feat: Vista de usuarios

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,13 +1,55 @@
 <template>
-  <div></div>
+  <div class="text-white w-full ">
+    <h1 class="text-center text-3xl mt-1 font-semibold">
+      Usuarios
+    </h1>
+    <ListUsers :users="users" />
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import { mapState } from "vuex";
+// Componentes
+import ListUsers from "@/components/users/ListUsers.vue";
+
 export default {
-  name: 'Users'
-}
+  name: "Users",
+  data: () => {
+    return {
+      users: [],
+    };
+  },
+  components: {
+    ListUsers,
+  },
+  computed: {
+    ...mapState(["API_URL", "headers"]),
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      await axios({
+        method: "get",
+        url: this.API_URL + "/user",
+        headers: this.headers,
+      })
+        .then((res) => {
+          if(Array.isArray(res.data)){
+            this.users = res.data;
+            console.log(this.users);
+          }else{
+            console.log(res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
