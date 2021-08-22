3c78ae1f98c605901024edf2e2598efcbc0e7087
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-22 20:54:00 -0300

    refactor: Moviendo funciones de sesiones al modulo auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,59 +1,22 @@
 import { createStore } from "vuex";
-import router from "@/router/index";
-import axios from "axios";
+
+
+
+// Modulos
+import auth from './modules/auth';  
+/* import usersPending from './modules/usersPending'; */
 
 export default createStore({
+  modules:{
+    auth
+  },
   state: {
-    API_URL: process.env.VUE_APP_ROOT_API,
-    token: null,
-    headers: {
-      Authorization: "",
-      "Content-Type": "application/json",
-    },
+    
   },
   mutations: {
-    setToken(state, payload) {
-      state.token = payload;
-    },
-    setHeaderToken(state, payload) {
-      state.headers.Authorization = payload;
-    }
+
   },
   actions: {
-    searcher({ commit }, payload) {
-      commit("setText", payload.toLowerCase());
-    },
-    syncToken({ commit }) {
-      if (localStorage.getItem("token")) {
-        commit("setToken", localStorage.getItem("token"));
-        commit("setHeaderToken", "Bearer " + localStorage.getItem("token"));
-      } else {
-        /* router.push("login"); */
-        console.log();
-      }
-    },
-    logout({ commit }) {
-      commit("setToken", null);
-      commit("setHeaderToken", "");
-      localStorage.removeItem("token");
-      router.push("login");
-    },
-    async checkSession({ state,  dispatch }) {
-      await axios({
-        method: "post",
-        url: state.API_URL + "/token",
-        data: {},
-        headers: state.headers,
-      })
-        .then((res) => {
-          console.log(res);
-          if (res.data != "OK") {
-            dispatch('logout');
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+    
   }
 });
