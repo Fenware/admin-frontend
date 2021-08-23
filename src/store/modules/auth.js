dbcb0befe1a05d51a6e327955e2bb26b832b62eb
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-22 21:32:33 -0300

    fix: Moviendo variables que no eran del modulo

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -5,47 +5,35 @@ import router from "@/router/index";
 import showAlert from "@/utils/alerts";
 
 export default {
-  state: {
-    API_URL: process.env.VUE_APP_ROOT_API,
-    token: null,
-    headers: {
-      Authorization: "",
-      "Content-Type": "application/json",
-    },
-  },
-  mutations: {
-    setToken(state, payload) {
-      state.token = payload;
-    },
-    setHeaderToken(state, payload) {
-      state.headers.Authorization = payload;
-    },
-  },
   actions: {
     syncToken({ commit }) {
       if (localStorage.getItem("token")) {
         commit("setToken", localStorage.getItem("token"));
         commit("setHeaderToken", "Bearer " + localStorage.getItem("token"));
-      } else {
-        console.log("");
       }
     },
     logout({ commit }) {
+      // La variable token la seteo a null
       commit("setToken", null);
       commit("setHeaderToken", "");
+      // Quito el valor del localStorage
       localStorage.removeItem("token");
+      // Redirecciono al login
       router.push("login");
     },
-    async checkSession({ state, dispatch }) {
+    // Funcion para verificar si la sesion esta bien
+    async checkSession({ rootState, dispatch }) {
       await axios({
         method: "post",
-        url: state.API_URL + "/token",
+        url: rootState.API_URL + "/token",
         data: {},
-        headers: state.headers,
+        headers: rootState.headers,
       })
         .then((res) => {
           console.log(res);
+          // Si res.data no es "OK" significa que la sesion expiró o el token esta mal o no hay token
           if (res.data != "OK") {
+            // LLamo a la funcion logout
             dispatch("logout");
           }
         })
@@ -53,24 +41,28 @@ export default {
           console.log(error);
         });
     },
-    async login({ state, commit, dispatch }, user) {
+    async login({ rootState, commit, dispatch }, user) {
       await axios({
         method: "post",
-        url: state.API_URL + "/auth",
+        url: rootState.API_URL + "/auth",
         data: user,
         headers: {
           "Content-Type": "application/json",
         },
       })
         .then((res) => {
+          // Si el token es un string significa que es correcto
           if (typeof res.data.result.token == "string") {
             let token = res.data.result.token;
+            // Le meto el token a la variable del state
             commit("setToken", token);
+            // Añado el item token al localStorage para guardarlo en el navegador
             localStorage.setItem("token", token);
+            // Sincronizo el token y los headers
             dispatch("syncToken");
+            // Redirigo al inicio
             router.push("/inicio");
           } else {
-            console.log("Error: login");
             showAlert({type: "error", message: res.data.result.error_msg});
           }
         })
