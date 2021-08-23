234f832dda2e617ef0c0e83bcd6be72bc7717c28
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-22 21:33:40 -0300

    fix: Devolviendo variables de headers y token al state global

--- src/store/index.js
+++ src/store/index.js
@@ -11,10 +11,20 @@ export default createStore({
     auth
   },
   state: {
-    
+    API_URL: process.env.VUE_APP_ROOT_API,
+    token: null,
+    headers: {
+      Authorization: "",
+      "Content-Type": "application/json",
+    },
   },
   mutations: {
-
+    setToken(state, payload) {
+      state.token = payload;
+    },
+    setHeaderToken(state, payload) {
+      state.headers.Authorization = payload;
+    },
   },
   actions: {
     
