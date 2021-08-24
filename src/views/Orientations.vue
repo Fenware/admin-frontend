ed854c2a12a7d93ac3cecee6f6fd28f3cd45cccf
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-23 22:06:54 -0300

    refactor: Funciones y variables movidas al modulo vuex

--- src/views/Orientations.vue
+++ src/views/Orientations.vue
@@ -4,12 +4,7 @@
       Orientaciones
     </h2>
 
-    <ListOrientation
-      v-show="mode == 'list'"
-      :orientations="orientations"
-      @changeMode="changeMode"
-      @deleteOrientation="deleteOrientation"
-    />
+    <ListOrientation v-show="mode == 'list'" />
 
     <CreateOrientation
       v-if="mode == 'create'"
@@ -27,19 +22,13 @@
 </template>
 
 <script>
-import axios from "axios";
-import { mapState, /* mapMutations,  */mapActions } from "vuex";
+import { mapState, mapActions } from "vuex";
 import ListOrientation from "@/components/orientations/ListOrientation";
 import CreateOrientation from "@/components/orientations/CreateOrientation";
 import EditOrientation from "@/components/orientations/EditOrientation";
 
 export default {
   name: "Orientations",
-  data: function() {
-    return {
-      mode: "list",
-    };
-  },
   created() {
     this.getOrientations();
   },
@@ -50,72 +39,11 @@ export default {
   },
   computed: {
     ...mapState({
-      orientations: state => state.orientations.orientations,
-      orientation: state => state.orientations.orientation,
+      mode: (state) => state.orientations.mode,
     }),
   },
   methods: {
-    /* ...mapMutations([]), */
-    ...mapActions(['getOrientations']),
-    changeMode(mode, orientation) {
-      this.mode = mode;
-      if (orientation) {
-        this.orientation = orientation;
-      }
-    },
-    modifyOrientation(modified_orientation) {
-      this.orientations.forEach((orientation) => {
-        if (orientation.id == modified_orientation.id) {
-          orientation.name = modified_orientation.name;
-          orientation.year = modified_orientation.year;
-        }
-      });
-      this.$swal({
-        icon: "success",
-        title: `La orientación ${modified_orientation.name} fue modificada correctamente!`,
-      });
-    },
-
-    addOrientation(orientation) {
-      this.orientations.push(orientation);
-    },
-    async deleteOrientation(id, orientation_name) {
-      var data = {
-        id: parseInt(id),
-      };
-      console.log(data);
-      await axios({
-        method: "delete",
-        url: this.API_URL + "/orientacion",
-        data: data,
-        headers: this.headers,
-      })
-        .then((res) => {
-          // Si la consulta salio bien
-          if (res.data == 1) {
-            // Elimino el objeto del array
-            this.orientations.forEach((orientation, index) => {
-              if (parseInt(orientation.id) == parseInt(id)) {
-                this.orientations.splice(index, 1);
-              }
-            });
-
-            // Lanzando alerta
-            this.$swal({
-              icon: "info",
-              title: `La orientación ${orientation_name} fue eliminada correctamente!`,
-            });
-          } else {
-            this.$swal({
-              icon: "error",
-              title: `La orientación ${orientation_name} no pudo ser eliminada, actualice la pagina e intente nuevamente`,
-            });
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+    ...mapActions(["getOrientations"]),
   },
 };
 </script>
