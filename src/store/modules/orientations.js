a39987b608b9b32140d0079a3a14244e83315fdd
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-23 21:41:04 -0300

    feat: Obteniendo orientaciones

--- src/store/modules/orientations.js
+++ src/store/modules/orientations.js
@@ -1,4 +1,32 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 import showAlert from "@/utils/alerts.js";
-export default {};
+export default {
+  state: {
+    orientations: [],
+    orientation: {},
+  },
+  getters: {},
+  mutations: {
+    setOrientations(state, orientations){
+      state.orientations = orientations;
+    }
+  },
+  actions: {
+    async getOrientations({rootState, commit}) {
+      await axios({
+        method: "get",
+        url: rootState.API_URL + "/orientacion",
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (Array.isArray(res.data)) {
+            commit("setOrientations", res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
