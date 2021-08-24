ff7656efaf50bb8435db62e88d345ec870cdad17
lucaspintos909 <lucaspintos909@gmail.com>
2021-08-24 11:32:23 -0300

    feat: Acciones para editar orientaciones

--- src/store/modules/orientations.js
+++ src/store/modules/orientations.js
@@ -4,9 +4,11 @@ import showAlert from "@/utils/alerts.js";
 export default {
   state: {
     mode: "list",
-    subjects: [],
-    orientations: [],
     orientation: {},
+    orientations: [],
+    subjects: [],
+    // Materias que ya estan seleccionadas al obtener la orientacion
+    preselected_subjects: [],
   },
   getters: {},
   mutations: {
@@ -16,10 +18,13 @@ export default {
     setSubjects(state, subjects) {
       state.subjects = subjects;
     },
-    changeMode(state, mode, orientation) {
-      state.mode = mode;
-      if (orientation) {
-        state.orientation = orientation;
+    setOrientationSubjects(state, subjects) {
+      state.preselected_subjects = subjects;
+    },
+    changeMode(state, data) {
+      state.mode = data.mode;
+      if (data.orientation) {
+        state.orientation = data.orientation;
       }
     },
     removeOrientation(state, orientation_id) {
@@ -131,6 +136,86 @@ export default {
               type: "error",
               message: `La orientación ${orientation.name} no pudo ser eliminada, actualice la pagina e intente nuevamente`,
             });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async getOrientationSubjects({ rootState, state, commit }) {
+      let data = `id=${state.orientation.id}`;
+      await axios({
+        method: "get",
+        url: rootState.API_URL + `/orientacion-materia?${data}`,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          // Verifico que la data recibida sea un array
+          if (Array.isArray(res.data)) {
+            commit("setOrientationSubjects", res.data);
+          }else{
+            console.log(res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async editOrientation({ rootState, commit }, modified_orientation) {
+      let data = modified_orientation;
+      data.id = parseInt(data.id);
+      await axios({
+        method: "put",
+        url: rootState.API_URL + "/orientacion",
+        data: data,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (res.data == 1) {
+            commit("modifyOrientation", modified_orientation);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async removeSubjectsOrientation({ rootState, state, commit }, removed_subjects) {
+      let data = {
+        id: parseInt(state.orientation.id),
+        subjects: removed_subjects,
+      };
+      console.log(data);
+      await axios({
+        method: "delete",
+        url: rootState.API_URL + "/orientacion-materia",
+        data: data,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (res.data == 1) {
+            console.log("Midificadi corritaminti");
+            /* commit("modifyOrientation", this.modified_orientation); */
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async addSubjectsOrientation({ rootState, state, commit }, added_subjects) {
+      let data = {
+        id: parseInt(state.orientation.id),
+        subjects: added_subjects,
+      };
+      await axios({
+        method: "post",
+        url: rootState.API_URL + "/orientacion-materia",
+        data: data,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (res.data == 1) {
+            /* this.$emit("changeOrientation", this.modified_orientation); */
+            console.log("Midificadi corritaminti");
           }
         })
         .catch((error) => {
