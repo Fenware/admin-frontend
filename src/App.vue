795c84f0a6a015a9daf91fc6401f74b7191b542d
lucaspintos909 <lucaspintos909@gmail.com>
2021-09-10 16:42:58 -0300

    feat: Clases para iconos y botones

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,52 @@ body {
   height: 100vh;
   width: 100vw;
 }
+
+.material-icons {
+  font-family: 'Material Icons Round';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
+
+/* ----------------- Buttons ----------------- */
+
+.btn-success {
+  @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-green-500 border-green-400 bg-green-200 hover:bg-green-300 text-green-900 cursor-pointer;
+}
+.btn-danger {
+  @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-red-500 border-red-400 bg-red-200 hover:bg-red-300 text-red-900 cursor-pointer;
+}
+.btn-warning {
+  @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-yellow-500 border-yellow-400 bg-yellow-200 hover:bg-yellow-300 text-yellow-900 cursor-pointer;
+}
+.btn-info {
+  @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-gray-500 border-indigo-400 bg-indigo-200 hover:bg-indigo-300 text-blue-900 cursor-pointer;
+}
+.btn-disabled {
+  @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2  border-gray-400 bg-gray-200 text-gray-900 cursor-default;
+}
+
+/* --------------------------------------------- */
+
 .h-85vh {
   height: 90vh;
 }
