f2bae7994b1eb6634ba9ceab93362af2711a4d66
lucaspintos909 <lucaspintos909@gmail.com>
2021-10-08 21:15:39 -0300

    feat: Separando vistas por usuario y usuarios

---
+++ src/views/User.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="text-white">
+    <h1 class="text-center text-3xl mt-1 font-semibold">Usuario</h1>
+    <div
+      v-if="'id' in edited_user"
+      class="max-w-screen-sm mx-auto mt-2 text-white bg-gray-600 bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl rounded-2xl
+    "
+    >
+      <div
+        class="
+        flex
+        justify-between
+        pl-5
+        items-center
+        bg-gray-200 bg-opacity-10
+        backdrop-filter backdrop-blur-xl
+        shadow-2xl
+        rounded-t-2xl
+      "
+      >
+        <div class="flex items-center">
+          <span class="material-icons">manage_accounts</span>
+          <h2 class="px-2 font-extrabold select-none">Perfil del usuario</h2>
+        </div>
+        <div class="flex items-center">
+          <!-- <button
+          @click="createUser()"
+          class="px-3 m-1 py-1 text-xs font-semibold transition-colors rounded-md bg-indigo-200 hover:bg-indigo-300 text-blue-900"
+        >
+          Guardar
+        </button> -->
+
+          <button
+            @click="changeModeToList()"
+            class="btn-info rounded-tr-xl px-2 m-1 py-1 border-0 text-xs flex items-center"
+          >
+            <span class="material-icons text-sm mr-1">arrow_back</span>
+
+            Volver
+          </button>
+        </div>
+      </div>
+
+      <!-- Cuerpo__________________________ -->
+
+      <div
+        class="p-3 border-b-2 border-l-2 border-r-2 border-gray-700 rounded-b-2xl"
+      >
+        <h1 class="text-2xl text-indigo-300 font-bold text-center">
+          {{ edited_user.type == "student" ? "ESTUDIANTE" : "DOCENTE" }}
+        </h1>
+
+        <div class="grid grid-cols-2 gap-2 px-10">
+          <div class="mb-2 mt-2 col-span-2 flex items-center">
+            <img
+              class="w-3/12 mr-5"
+              :src="require('@/assets/avatars/' + edited_user.avatar)"
+              alt="avatar"
+            />
+            <div class="w-full">
+              <div class="block">
+                <span class="block select-none text-xs "
+                  >Cédula de identidad</span
+                >
+                <p class="font-medium text-2xl tracking-widest px-2">
+                  {{ edited_user.ci }}
+                </p>
+              </div>
+
+              <div class="block mt-2 ">
+                <label for="nickname" class="block select-none text-xs"
+                  >Nombre de usuario</label
+                >
+                <input
+                  id="nickname"
+                  class="input w-full"
+                  type="text"
+                  v-model="edited_user.nickname"
+                  :placeholder="
+                    edited_user.nickname
+                      ? 'Ingrese el nombre de usuario'
+                      : 'Ninguno'
+                  "
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <label for="name" class="block select-none text-xs">Nombre</label>
+            <input
+              id="name"
+              class="input"
+              type="text"
+              v-model="edited_user.name"
+            />
+          </div>
+
+          <div>
+            <label for="middle_name" class="block select-none text-xs"
+              >Segundo nombre</label
+            >
+            <input
+              id="middle_name"
+              class="input"
+              type="text"
+              v-model="edited_user.middle_name"
+              :placeholder="
+                !edited_user.middle_name
+                  ? 'Ninguno'
+                  : 'Ingrese el segundo nombre'
+              "
+            />
+          </div>
+          <div>
+            <label for="surname" class="block select-none text-xs"
+              >Apellido</label
+            >
+            <input
+              id="surname"
+              class="input"
+              type="text"
+              v-model="edited_user.surname"
+            />
+          </div>
+
+          <div>
+            <label for="second_surname" class="block select-none text-xs"
+              >Segundo apellido</label
+            >
+            <input
+              id="second_surname"
+              class="input"
+              type="text"
+              v-model="edited_user.second_surname"
+              :placeholder="
+                !edited_user.second_surname
+                  ? 'Ninguno'
+                  : 'Ingrese el segundo apellido'
+              "
+            />
+          </div>
+          <div class=" col-span-2 mt-">
+            <label for="email" class="block select-none text-xs">Email</label>
+            <input
+              id="email"
+              class="input w-full"
+              type="email"
+              v-model="edited_user.email"
+              :placeholder="
+                !edited_user.email ? 'Ninguno' : 'Ingrese el segundo apellido'
+              "
+            />
+          </div>
+        </div>
+
+        <div class="flex justify-center mb-2 col-span-2 mt-5 ">
+          <button
+            class="px-3 py-0.5 min-w-max text-sm font-semibold rounded-md transition-colors hover:bg-indigo-200 hover:text-blue-900 cursor-pointer"
+          >
+            Historial de consultas
+          </button>
+        </div>
+
+        <div class="flex justify-between mt-3 gap-2">
+          <button
+            @click="saveChanges()"
+            :disabled="!wasEdited"
+            :class="wasEdited ? 'btn-success' : 'btn-disabled'"
+            class="text-sm mt-1 px-3 flex items-center"
+          >
+            <span class="mt-0.5">Guardar cambios</span>
+          </button>
+
+          <button
+            @click="removeUser()"
+            class="btn-danger text-sm mt-1 pt-0.5 px-3 flex items-center"
+          >
+            <span class="material-icons text-md mr-1">warning</span>
+            <span class="mt-0.5">Dar de baja</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+import { confirmModal, showAlert } from "@/utils/alerts.js";
+
+export default {
+  name: "User",
+  data: function() {
+    return {
+      edited_user: {
+        avatar: "01-man.svg",
+        name: "Esperando datos",
+        middle_name: "Esperando datos",
+        surname: "Esperando datos",
+        email: "Esperando datos",
+        nickname: "Esperando datos",
+      },
+    };
+  },
+  created() {
+    this.getUserByNickname(this.$route.params.nickname).then(() => {
+      if (
+        "id" in this.user &&
+        this.user.nickname == this.$route.params.nickname
+      ) {
+        this.edited_user = { ...this.user };
+        this.edited_user.id = parseInt(this.edited_user.id);
+      }
+    });
+  },
+  computed: {
+    ...mapState({
+      user: (state) => state.users.user,
+    }),
+    wasEdited() {
+      return (
+        this.user.name != this.edited_user.name ||
+        this.user.middle_name != this.edited_user.middle_name ||
+        this.user.surname != this.edited_user.surname ||
+        this.user.second_surname != this.edited_user.second_surname ||
+        this.user.nickname != this.edited_user.nickname ||
+        this.user.email != this.edited_user.email
+      );
+    },
+  },
+  methods: {
+    ...mapActions(["deleteUser", "editUser", "getUserByNickname"]),
+    validateData() {
+      if (this.edited_user.name.length == 0) {
+        showAlert({
+          type: "error",
+          message: `Debes ingresar un nombre`,
+        });
+        return false;
+      } else if (this.edited_user.surname.length == 0) {
+        showAlert({
+          type: "error",
+          message: `Debes ingresar un apellido`,
+        });
+        return false;
+      } else if (this.edited_user.email.length == 0) {
+        showAlert({
+          type: "error",
+          message: `Debes ingresar un email`,
+        });
+        return false;
+      } else if (this.edited_user.nickname.length == 0) {
+        showAlert({
+          type: "error",
+          message: `Debes ingresar un nombre de usuario`,
+        });
+        return false;
+      } else {
+        return true;
+      }
+    },
+    removeUser() {
+      confirmModal({
+        function: this.deleteUser,
+        second_function: this.changeModeToList,
+        text: `<span class="text-white"><b>CI: ${this.user.ci}<br> ${this.user.name} ${this.user.surname}  </b> <br> ¿Dar de baja al usuario?</span>`,
+        data: {
+          user_id: parseInt(this.user.id),
+          username: `${this.user.name} ${this.user.surname}`,
+        },
+      });
+    },
+    saveChanges() {
+      if (this.validateData()) this.editUser(this.edited_user);
+    },
+  },
+};
+</script>
+
+<style>
+.w-90per {
+  width: 90%;
+}
+.input {
+  @apply w-90per font-medium px-2 py-0.5 transition-all bg-opacity-0 focus:bg-opacity-10 hover:bg-opacity-10 bg-white rounded-md text-xl outline-none;
+}
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,46 +1,193 @@
 <template>
   <div class="text-white w-full">
     <h1 class="text-center text-3xl mt-1 font-semibold">Usuarios</h1>
+
+    <div
+      class="my-2 text-white bg-gray-600 bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl rounded-2xl"
+    >
+      <div
+        class="flex justify-between pl-3 items-center bg-gray-200 bg-opacity-10 backdrop-filter backdrop-blur-xl shadow-2xl rounded-t-2xl"
+      >
+        <div class="flex items-center">
+          <span class="material-icons">people</span>
+          <h2 class="p-1 font-extrabold">Lista de usuarios</h2>
+        </div>
+        <div class="flex items-center mr-5">
+          <span class="material-icons mr-2">filter_list</span>
+
+          <!-- Al darle click cambia la variable del filtro y se le agregan las clases para que quede "seleccionado" el boton -->
+          <button
+            @click="filter_by = 'all'"
+            :class="
+              'text-sm px-2 py-0.5 transition-colors rounded-lg ' +
+                (filter_by == 'all' ? '  bg-white bg-opacity-20 ' : '')
+            "
+          >
+            Todos
+          </button>
+
+          <!-- Al darle click cambia la variable del filtro y se le agregan las clases para que quede "seleccionado" el boton -->
+          <button
+            @click="filter_by = 'teacher'"
+            :class="
+              'text-sm px-2 py-0.5 transition-colors rounded-lg ' +
+                (filter_by == 'teacher' ? ' bg-white bg-opacity-20 ' : '')
+            "
+          >
+            Docentes
+          </button>
+
+          <!-- Al darle click cambia la variable del filtro y se le agregan las clases para que quede "seleccionado" el boton -->
+          <button
+            @click="filter_by = 'student'"
+            :class="
+              'text-sm px-2 py-0.5 transition-colors rounded-lg ' +
+                (filter_by == 'student' ? ' bg-white bg-opacity-20 ' : '')
+            "
+          >
+            Estudiantes
+          </button>
+        </div>
+      </div>
+      <div
+        class="flex justify-center px-5 border-l-2 border-r-2 pt-5 border-gray-700"
+      >
+        <!-- Mostrando el input solo si hay usuarios pendientes para buscar -->
+        <input
+          type="text"
+          placeholder="Buscar por nombre o cédula"
+          v-model="text_filter"
+          class="w-1/3 focus:w-2/5 placeholder-gray-300 text-center py-2 px-2 | bg-white transition-all transform duration-300 focus:bg-opacity-20 hover:bg-opacity-20 bg-opacity-10 shadow-xl | rounded-xl  outline-none"
+        />
+      </div>
+      <div
+        class="py-3 border-b-2 border-l-2 border-r-2 border-gray-700 rounded-b-2xl"
+      >
+        <div
+          class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 max-h-96 overflow-auto "
+        >
+          <!-- Haciendo un for de los usuarios filtrados (por defecto se muestran todos) -->
+          <div
+            v-for="user in usersFiltered"
+            :key="user.id"
+            class="sm:flex sm:justify-between my-2 mx-3 py-2 px-3 bg-gray-700 border-2 border-gray-600 shadow-md rounded-2xl"
+          >
+            <div>
+              <p
+                class="mb-1 text-xs tracking-widest font-extrabold select-none text-indigo-300"
+              >
+                <!-- Poniendo si es estudiante o docente segun el tipo -->
+                {{ user.type == "student" ? "ESTUDIANTE" : "DOCENTE" }}
+              </p>
+
+              <p class="font-bold text-xl">
+                {{ user.name }}
+                {{ user.middle_name }} {{ user.surname }}
+                {{ user.second_surname }}
+              </p>
+              <p>
+                <span class="font-medium">CI: </span
+                ><span class=" tracking-widest">{{ user.ci }}</span>
+              </p>
+              <!-- <p>
+              <span class="font-medium">Nickname: </span>{{ user.nickname }}
+            </p> -->
+              <p class="flex items-center">
+                <span class="font-medium">{{ user.email }}</span>
+                <span
+                  class="material-icons text-xl ml-1 text-gray-400 cursor-pointer transition-colors hover:text-gray-300"
+                  >content_copy</span
+                >
+              </p>
+            </div>
+
+            <div
+              class="flex md:flex-col flex-wrap gap-2 justify-center md:justify-start"
+            >
+              <router-link
+                :to="{ name: 'User', params: { nickname: user.nickname } }"
+                class="btn-info pr-3 mt-1 pl-5 font-semibold flex items-center"
+              >
+                Ver más
+                <span class="material-icons">expand_more</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- Muestro el componente para listar usuarios si la variable mode == 'list'
          Le paso por props el array de usuarios
          Le puedo pasar una funcion de este componente para que la use desde el componente hijo -->
-    <ListUsers
+    <!-- <ListUsers
       v-show="mode == 'list'"
       @changeMode="changeMode"
     />
 
-    <EditUser v-if="mode == 'edit'" @changeMode="changeMode" />
+    <EditUser v-if="mode == 'edit'" @changeMode="changeMode" /> -->
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
-// Componentes
-import ListUsers from "@/components/users/ListUsers.vue";
-import EditUser from "@/components/users/EditUser.vue";
+import { mapActions, mapMutations, mapState } from "vuex";
 
 export default {
   name: "Users",
   data: () => {
     return {
-      // El modo esta por defecto en listar usuario
-      mode: "list",
+      filter_by: "all",
+      text_filter: "",
     };
-  },
-  components: {
-    ListUsers,
-    EditUser
   },
   created() {
     this.getUsers();
   },
+  computed: {
+    ...mapState({
+      users: (state) => state.users.users,
+    }),
+    usersFiltered() {
+      // Filtrando siempre por tipo de usuario
+      let users_filtered =
+        this.filter_by == "all"
+          ? this.users.filter((user) => user.state_account == "1")
+          : this.users.filter(
+              (user) => user.type == this.filter_by && user.state_account == "1"
+            );
+
+      // Si el filtro es un numero ( osea una cedula )
+      if (!isNaN(parseInt(this.text_filter))) {
+        // Filtra los usuarios por coincidencias de cedula
+        users_filtered = users_filtered.filter(
+          (user) => user.ci.indexOf(this.text_filter.toString()) >= 0
+        );
+
+        // Si no es un numero y lo ingresado no son espacios
+      } else if (
+        isNaN(parseInt(this.text_filter)) &&
+        this.text_filter.trim() != ""
+      ) {
+        users_filtered = users_filtered.filter((user) => {
+          // Concateno nombres y apellidos
+          let nombre =
+            user.name +
+            " " +
+            user.middle_name +
+            " " +
+            user.surname +
+            " " +
+            user.second_surname;
+
+          // Filtro usuario si hay coincidencias de lo ingresado con el nombre completo
+          return nombre.indexOf(this.text_filter.toString()) >= 0;
+        });
+      }
+      return users_filtered;
+    },
+  },
   methods: {
     ...mapActions(["getUsers"]),
-    // Esta funcion la uso para cambiar la visibilidad de los componentes (listar, editar, crear, etc)
-    changeMode(mode) {
-      // Si necesitas pasar datos desde el hijo hacia acá podes agregarle parametros y setearselos a una variable en data ted dejo comentado el ejemplo
-      this.mode = mode;
-    },
+    ...mapMutations(["setUser"]),
   },
 };
 </script>
