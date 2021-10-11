7975b5309c192381bdd80c52a9a5cd106c57d0ba
lucaspintos909 <lucaspintos909@gmail.com>
2021-10-10 21:40:06 -0300

    feat: Funciones para obtener consulta/s

--- src/store/modules/consultations.js
+++ src/store/modules/consultations.js
@@ -6,7 +6,7 @@ import axios from "axios";
 export default {
   state: {
     user_consultations: [],
-    consultations: {},
+    consultation: {},
   },
   mutations: {
     setUserConsultations(state, consultations) {
@@ -15,11 +15,20 @@ export default {
     setConsultation(state, consultation) {
       state.consultation = consultation;
     },
+    clearConsultation(state){
+      state.consultation = {};
+    },
+    setConsultationBody(state, body) {
+      state.consultation.body = body;
+    },
+    setConsultationMessages(state, messages) {
+      state.consultation.messages = messages;
+    },
   },
   actions: {
-    async getUserConsultations({ commit, rootState }, user_id) {
+    async getUserConsultations({ commit, rootState }) {
       let data = {
-        user: parseInt(user_id)
+        user: parseInt(rootState.users.user.id)
       }
       await axios({
         method: "post",
@@ -33,6 +42,53 @@ export default {
             commit("setUserConsultations", res.data);
           } else {
             console.log(res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async getConsultation({ rootState, commit, dispatch }, id) {
+      let data = { consulta: parseInt(id) };
+      console.log(data);
+      await axios({
+        method: "post",
+        url: rootState.API_URL + `/consultation/getConsultationById`,
+        data,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (!("result" in res.data)) {
+            res.data.messages = [];
+            res.data.state = parseInt(res.data.state);
+            res.data.active = true;
+            commit("setConsultation", res.data);
+            dispatch("getConsultationMessages", id);
+          } else {
+            console.log("Error: getConsultation ->" + res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async getConsultationMessages({ rootState, commit }, id) {
+      let data = { consulta: parseInt(id) };
+      await axios({
+        method: "post",
+        url: rootState.API_URL + `/consultation/getMessages`,
+        data,
+        headers: rootState.headers,
+      })
+        .then((res) => {
+          if (Array.isArray(res.data)) {
+            console.log(res.data);
+            let body = res.data[0].content;
+            commit("setConsultationBody", body);
+            res.data.splice(0, 1);
+            commit("setConsultationMessages", res.data);
+          } else {
+            console.log("Error: getConsultationMessages ->" + res.data);
           }
         })
         .catch((error) => {
