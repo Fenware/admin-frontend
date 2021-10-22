063ca6ae6bd35767af30c794adadf6075d269cab
lucaspintos909 <lucaspintos909@gmail.com>
2021-10-22 11:41:17 -0300

    feat: Añadiendo animacion de modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="" v-if="currentRouteName() == 'Login'">
+  <div v-if="currentRouteName() == 'Login'" class="">
     <router-view/>
   </div>
   <div
-    class="flex items-center min-h-screen max-h-screen max-w-screen"
     v-if="currentRouteName() != 'Login'"
+    class="flex items-center min-h-screen max-h-screen max-w-screen"
   >
     <div
       class=" flex flex-col sm:flex-row sm:justify-between h-85vh w-95per mx-auto bg-gray-700 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-md "
     >
-      <Navbar />
+      <Navbar/>
       <Main class="mt-5 sm:mt-0"/>
       <!-- <Sidebar /> -->
     </div>
@@ -51,15 +51,11 @@ body {
   width: 100vw;
 }
 
-.form-input{
-  @apply text-lg bg-white bg-opacity-20 hover:bg-opacity-30 focus:bg-opacity-30 transition-all outline-none rounded-lg px-2 py-2 w-full;
-}
-
 .material-icons {
   font-family: 'Material Icons Round';
   font-weight: normal;
   font-style: normal;
-  font-size: 24px;  /* Preferred icon size */
+  font-size: 24px; /* Preferred icon size */
   display: inline-block;
   line-height: 1;
   text-transform: none;
@@ -85,15 +81,19 @@ body {
 .btn-success {
   @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-green-500 border-green-400 bg-green-200 hover:bg-green-300 text-green-900 cursor-pointer;
 }
+
 .btn-danger {
   @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-red-500 border-red-400 bg-red-200 hover:bg-red-300 text-red-900 cursor-pointer;
 }
+
 .btn-warning {
   @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-yellow-500 border-yellow-400 bg-yellow-200 hover:bg-yellow-300 text-yellow-900 cursor-pointer;
 }
+
 .btn-info {
   @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2 hover:border-gray-500 border-indigo-400 bg-indigo-200 hover:bg-indigo-300 text-blue-900 cursor-pointer;
 }
+
 .btn-disabled {
   @apply px-3 py-1 min-w-max font-semibold rounded-md transition-colors border-b-2  border-gray-400 bg-gray-200 text-gray-900 cursor-default;
 }
@@ -103,9 +103,11 @@ body {
 .h-85vh {
   height: 90vh;
 }
+
 .w-95per {
   width: 95%;
 }
+
 ::-webkit-scrollbar {
   width: 5px;
   height: 6px;
@@ -119,16 +121,60 @@ body {
   background: rgb(242, 242, 242);
   border-radius: 1rem;
 }
+
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.05s ease;
+  transition: opacity 0.08s ease-in-out;
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
 }
+
 * {
-    -webkit-tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.animated {
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+.animated.faster {
+  -webkit-animation-duration: 500ms;
+  animation-duration: 500ms;
+}
+
+.fadeIn {
+  -webkit-animation-name: fadeIn;
+  animation-name: fadeIn;
+}
+
+.fadeOut {
+  -webkit-animation-name: fadeOut;
+  animation-name: fadeOut;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
 }
 </style>
